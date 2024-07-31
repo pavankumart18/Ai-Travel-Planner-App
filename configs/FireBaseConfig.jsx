facda9006488cbcfe32e44e4c4d161365418e16e
Pavan Kumar <115294457+pavankumart18@users.noreply.github.com>
2024-07-31 22:29:13 +0530

    Update FireBaseConfig.jsx

--- configs/FireBaseConfig.jsx
+++ configs/FireBaseConfig.jsx
@@ -7,14 +7,7 @@ import { getReactNativePersistence ,initializeAuth } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore,collection } from "firebase/firestore";
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD_yppZOWbC3Rb9ClHSybiq7yyxTT5nhY8",
-  authDomain: "fir-chat-48a7d.firebaseapp.com",
-  projectId: "fir-chat-48a7d",
-  storageBucket: "fir-chat-48a7d.appspot.com",
-  messagingSenderId: "622042439081",
-  appId: "1:622042439081:web:400a94261e063db4ac78b8",
-};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
